ab5a0d426f8201165153d9f0bc1a1e55fc18c47f
Joonsung <gotohell.covid19@gmail.com>
2021-08-21 01:28:11 +0900

    Complete function in python file

--- breakModel_1.py
+++ breakModel_1.py
@@ -1,9 +1,82 @@
-"""
-cf) x의 n제곱근: x**(1.0/n)
-D = ((DP + r2)/(r1*r2))**(1.0/r3)
-"""
-
 # Python code to break the Honeycomb Security Model 1
+import math
+import sys
+
+def log(data, newData=""):
+    print("LOG: ", data + str(newData))
+
+def default(n1, n2, n3, dp_k, data):
+    print("INTRUDER KNOWS EVERY DATA EXCEPT FOR THE LOCATION OF 'K'")
+    print("\n")
+    print("CALCULATING...")
+    print("\nCALCULATION LOG")
+    print("\n")
+    # data_default = (((dp_m+min_r2)/(min_r2_data*n_list[1]))**(1.0/float(n_list[0])))
+    log("VALUE OF RENDERED 'DATA' = ", )
+    log("FETCHING THE MINIMUM POSSIBLE VALUE OF r2")
+    min_r2_data = min(n1, n2, n3)
+    log("SUCCESS, MIN VALUE = ", min_r2_data)
+    
+    log("EXPERIMENTING WITH MINIMUM VALUE ", min_r2_data)
+    min_r2 = 0
+    n_list = [n1, n2, n3]
+    n_list.remove(min_r2_data)
+
+    log("r1 = ", n_list[1])
+    log("r3 = ", n_list[0])
+    print("\n")
+    while (min_r2 < min_r2_data):
+        dp_m = dp_k
+        log("r2 = ", min_r2)
+        data_m = (((dp_m+min_r2)/(min_r2_data*n_list[1]))**(1.0/float(n_list[0])))
+        log("TRUE/FALSE - ", math.isclose(data_m, data))
+        if (math.isclose(data_m, data)) == True:
+            print("*********************************")
+            print("********** MATCH FOUND **********")
+            print("*********************************")
+            print("ORIGINAL DATA: ", data)
+            print("RENDERED DATA: ", data_m)
+            print("r1 = ", n_list[1])
+            print("r2 = ", min_r2_data)
+            print("r3 = ", n_list[0])
+            print("INTRUDER LOCATION = ", min_r2)
+            break
+        else:
+            log("VALUE OF data_m = ", data_m)
+            log("NO MATCH, MOVING ON")
+            print("\n")
+            min_r2 += 1
+    print("\n\n")
+
+    log("CHANGE r1, r3 VALUES")
+    min_r2 = 0
+    log("r1 = ", n_list[0])
+    log("r3 = ", n_list[1])
+    print("\n")
+    while (min_r2 < min_r2_data):
+        dp_m = dp_k
+        log("r2 = ", min_r2)
+        data_m = (((dp_m+min_r2)/(min_r2_data*n_list[0]))**(1.0/float(n_list[1])))
+        log("TRUE/FALSE - ", math.isclose(data_m, data))
+        if (math.isclose(data_m, data)) == True:
+            print("\n\n\n")
+            print("*****************************************************")
+            print("******************** MATCH FOUND ********************")
+            print("*****************************************************")
+            print("ORIGINAL DATA: ", data)
+            print("RENDERED DATA: ", data_m)
+            print("r1 = ", n_list[0])
+            print("r2 = ", min_r2_data)
+            print("r3 = ", n_list[1])
+            print("INTRUDER LOCATION = ", min_r2)
+            break
+        else:
+            log("VALUE OF data_m = ", data_m)
+            log("NO MATCH, MOVING ON")
+            print("\n")
+            min_r2 += 1
+    print("\n\n")    
+
 
 print("_______________________________________________________________________________")
 print("██╗░░██╗░█████╗░███╗░░██╗███████╗██╗░░░██╗░█████╗░░█████╗░███╗░░░███╗██████╗░")
@@ -38,50 +111,7 @@ print("CALCULATED VALUE OF 'DATA PRIME' = ", dp)
 print("\n\n")
 k = float(input("LENGTH FROM DATA ORIGIN TO 'K' = "))
 print("\n")
-dp_k = dp - k + 1
+dp_k = dp - k
 print("INTRUDER GETS 'DATA PRIME' VALUE AS = ", dp_k)
 
-default(r1, r2, r3, data)
-
-def log(data):
-    print("LOG: ", data)
-
-def default(n1, n2, n3, data):
-    print("INTRUDER KNOWS EVERY DATA EXCEPT FOR THE LOCATION OF 'K'")
-    print("\n")
-    print("CALCULATING...")
-    print("\nCALCULATION LOG")
-    
-    log("FETCHING THE MINIMUM POSSIBLE VALUE OF r2")
-    min_r2_data = min(n1, n2, n3)
-    log("SUCCESS, MIN VALUE = ", min_r2_data)
-    
-    log("EXPERIMENTING WITH MINIMUM VALUE ", min_r2_data)
-    min_r2 = 0
-    n_list = [n1, n2, n3]
-    n_list.remove(min_r2_data)
-
-    while (min_r2 < min_r2_data):
-        dp_m = dp_k + min_r2
-        log("r2 = ", min_r2_data)
-        log("r1 = ", n_list[1])
-        log("r3 = ", n_list[0])
-        data_m = (((dp_m + min_r2_data)/min_r2_data*n_list[1])**(1.0/float(n_list[0])))
-        if data_m = data:
-            log("***** MATCH FOUND *****")
-            log("data_m FOUND = ", data_m)
-            break
-        else:
-            log("NO MATCH, MOVING ON")
-            print("\n")
-            min_r2 += 1
-
-
-
-"""
-def calc(case):
-    if case = 1:
-        print("INTRUDER KNOWS VALUE OF r1, r2, r3")
-        print("BUT DOES NOT KNOW WHICH IS WHICH")
-        print("\n")
-"""
+default(r1, r2, r3, dp_k, data)
