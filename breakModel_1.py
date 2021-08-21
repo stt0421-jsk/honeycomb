832df3e6a62aba2823450b59b1958f3ee4fed953
Joonsung <gotohell.covid19@gmail.com>
2021-08-21 16:28:29 +0900

    Final version of breakModel_1.py

--- breakModel_1.py
+++ breakModel_1.py
@@ -2,6 +2,8 @@
 import math
 import sys
 import mpmath as mp
+from decimal import *
+
 
 
 def log(data, newData=""):
@@ -13,12 +15,13 @@ def default(n1, n2, n3, dp_k, data):
     print("CALCULATING...")
     print("\nCALCULATION LOG")
     print("\n")
-    log("MPMATH SETTINGS")
-    mp.prec = 500
-    mp.dps = 100
-    print(mp)
-    # data_default = (((dp_m+min_r2)/(min_r2_data*n_list[1]))**(1.0/float(n_list[0])))
-    log("VALUE OF RENDERED 'DATA' = ", )
+    # log("MPMATH SETTINGS")
+    # mp.prec = 500
+    # mp.dps = 100
+    # print(mp)
+    getcontext().prec = 2000
+    data_default = (((dp_k+n2)//(n2*n1))**(Decimal(1)/Decimal(r3)))
+    log("VALUE OF RENDERED 'DATA' = ", data_default)
     log("FETCHING THE MINIMUM POSSIBLE VALUE OF r2")
     min_r2_data = min(n1, n2, n3)
     log("SUCCESS, MIN VALUE = ", min_r2_data)
@@ -32,14 +35,18 @@ def default(n1, n2, n3, dp_k, data):
     log("r3 = ", n_list[0])
     print("\n")
     while (min_r2 < min_r2_data):
-        dp_m = dp_k
+        dp_m = int(dp_k)
+        #log("dp_m = ", dp_m)
         log("r2 = ", min_r2)
-        log("DP_M = ", dp_m)
-        data_m = (((dp_m+min_r2)/(min_r2_data*n_list[1]))**(1.0/float(n_list[0])))
-        if (math.isclose(data_m, data)) == True:
-            print("*********************************")
-            print("********** MATCH FOUND **********")
-            print("*********************************")
+        getcontext().prec = 2000
+        data_m = (((dp_m+(min_r2*(min_r2_data**min_r2_data)))//(min_r2_data*n_list[1]))**(Decimal(1)/Decimal(n_list[0])))
+        if (math.isclose(data_m, data, abs_tol=Decimal(0.000000000000000000000000000000000001))) == True:
+            print("\n")
+            print("*****************************************************")
+            print("*****************************************************")
+            print("******************** MATCH FOUND ********************")
+            print("*****************************************************")
+            print("*****************************************************")
             print("ORIGINAL DATA: ", data)
             print("RENDERED DATA: ", data_m)
             print("r1 = ", n_list[1])
@@ -50,7 +57,8 @@ def default(n1, n2, n3, dp_k, data):
             #break
         else:
             log("VALUE OF data_m = ", data_m)
-            log("NO MATCH, MOVING ON")
+            print("\n")
+            log("** NO MATCH, MOVING ON")
             print("\n")
             min_r2 += 1
     print("\n\n")
@@ -61,22 +69,28 @@ def default(n1, n2, n3, dp_k, data):
     log("r3 = ", n_list[1])
     print("\n")
     while (min_r2 < min_r2_data):
-        dp_m = dp_k
+        dp_m = int(dp_k)
+        #log("dp_m = ", dp_m)
         log("r2 = ", min_r2)
-        log("DP_M = ", dp_m)
-        log("DP_M * 2 = ", 2*dp_m)
-        data_m = (((dp_m+min_r2)/(min_r2_data*n_list[0]))**(1.0/float(n_list[1])))
-        if (math.isclose(data_m, data, rel_tol=0.000000000000009)) == True:
-            print("\n\n")
+        log("min_r2_data = ", min_r2_data)
+        log("n_list[1] = ", n_list[1])
+        log("n_list[0] = ", n_list[0])
+        getcontext().prec = 2000
+        data_m = (((dp_m+(min_r2*(min_r2_data**min_r2_data)))//(min_r2_data*n_list[0]))**(Decimal(1)/Decimal(n_list[1])))
+        if (math.isclose(data_m, data, abs_tol=Decimal(0.000000000000000000000000000000000001))) == True:
+            print("\n")
+            print("*****************************************************")
             print("*****************************************************")
             print("******************** MATCH FOUND ********************")
             print("*****************************************************")
-            print("ORIGINAL DATA: ", data)
-            print("RENDERED DATA: ", data_m)
-            print("r1 = ", n_list[0])
-            print("r2 = ", min_r2_data)
-            print("r3 = ", n_list[1])
-            print("INTRUDER LOCATION = ", min_r2)
+            print("*****************************************************")
+            print("\n")
+            print("** ORIGINAL DATA: ", data)
+            print("** RENDERED DATA: ", data_m)
+            print("** r1 = ", n_list[0])
+            print("** r2 = ", min_r2_data)
+            print("** r3 = ", n_list[1])
+            print("** INTRUDER LOCATION = ", min_r2)
             min_r2 += 1
             print("\n\n")
             #break
@@ -109,19 +123,19 @@ print("_________________________________________________________________________
 print("\n")
 print("\n")
 
-data = float(input("VALUE OF 'DATA' = "))
+data = int(input("VALUE OF 'DATA' = "))
 print("\n")
-r1 = float(input("LENGTH OF ROUTE 1 = "))
-r2 = float(input("LENGTH OF ROUTE 2 = "))
-r3 = float(input("LENGTH OF ROUTE 3 = "))
+r1 = int(input("LENGTH OF ROUTE 1 = "))
+r2 = int(input("LENGTH OF ROUTE 2 = "))
+r3 = int(input("LENGTH OF ROUTE 3 = "))
 print("\n")
 print("CALCULATING 'DATA PRIME'...")
-dp = float(r1*r2*(data**r3))
+dp = int(r1*r2*(data**r3))
 print("CALCULATED VALUE OF 'DATA PRIME' = ", dp)
 print("\n\n")
-k = float(input("LENGTH FROM DATA ORIGIN TO 'K' = "))
+k = int(input("LENGTH FROM DATA ORIGIN TO 'K' = "))
 print("\n")
-dp_k = dp - k
+dp_k = dp - k*(r2**r2)
 print("INTRUDER GETS 'DATA PRIME' VALUE AS = ", dp_k)
 
 default(r1, r2, r3, dp_k, data)
